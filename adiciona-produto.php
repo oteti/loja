9f6639a3332f2ee043018ab49578c4038d0a9dbe
Otávio Monteiro <35412224+oteti@users.noreply.github.com>
2018-01-15 21:03:17 -0200

    all clear

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,9 +1,24 @@
-<?php include 'cabecalho.php'; ?>
-  <?php
-  $nomeProduto = $_GET["nome"];
-  $preco = $_GET["preco"];
-   ?>
-  <p class="alert-success">
-     Produto <?php echo $nomeProduto; ?> de preço <?php echo $preco; ?> adicionado com sucesso!;
-  </p>
-<?php include 'rodape.php'; ?>
+<?php
+include ('cabecalho.php');
+
+        $nome = $_GET["nome"];
+        $preco = $_GET["preco"];
+
+        $query = "insert into produtos (nome,preco) values('{$nome}',{$preco})";
+        $conexao = mysqli_connect('localhost', 'root','','loja'); // ( ip , usuario(padrao 'root') , senha (padrao 'nula'), nome do banco de dados )
+
+        if (mysqli_query($conexao,$query)) { ?>
+
+            <p class="alert-success"> Produto <?php echo $nome; ?> de preço <?php echo $preco; ?> adicionado com sucesso!; </p>
+
+          <?php }
+           else { ?>
+
+            <p class="alert-danger"> Produto <?php echo $nome; ?> não foi adicionado com sucesso!; </p>
+
+          <?php }
+
+        mysqli_close($conexao);
+
+          ?>
+<?php include ('rodape.php'); ?>
