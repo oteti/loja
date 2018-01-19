d5fc125b3436f8c6835650bdfd4bfa34b8c109d0
Otávio Monteiro <35412224+oteti@users.noreply.github.com>
2018-01-19 11:03:43 -0200

    ok

--- produto-altera-formulario.php
+++ produto-altera-formulario.php
@@ -1,12 +1,12 @@
 <?php include ('cabecalho.php');
 include 'conecta.php';
 include 'banco-categoria.php';
+include 'banco-produto.php';
 
 $id = $_GET['id'];
-$produto = buscaID($conexao,$id){
-
-}
+$produto = buscaProduto($conexao,$id);
 $categorias = listaCategoria($conexao);
+$usado = $produto['usado'] ? "checked='checked'" : "";
  ?>
 
 <h1>Alterando produto</h1>
@@ -15,31 +15,33 @@ $categorias = listaCategoria($conexao);
       <tr>
           <td>Nome:</td>
           <td>
-             <input type="text" name="nome" class="form-control"></imput></br>
+             <input type="text" name="nome" class="form-control" value="<?=$produto['nome']?>"></imput></br>
            </td>
       </tr>
       <tr>
           <td>Preco:</td>
           <td>
-             <input type="number" name="preco" class="form-control"></input></br>
+             <input type="number" name="preco" class="form-control" value="<?=$produto['preco']?>"></input></br>
            </td>
       </tr>
       <tr>
           <td>Descrição;</td>
           <td>
-            <textarea name="descricao" class="form-control"></textarea></br>
+            <textarea name="descricao" class="form-control"><?=$produto['descricao']?></textarea></br>
           </td>
       </tr>
       <tr>
         <td></td>
-        <td> <input type="checkbox" name="usado" value="true"> Usado</td>
+        <td> <input type="checkbox" name="usado" value="true" <?=$usado?>> Usado</td>
       </tr>
       <tr>
           <td>Categoria</td>
           <td>
             <select class="form-control" name="categoria_id">
               <?php foreach ($categorias as $categoria): ?>
-                <option value="<?=$categoria['id']?>"> <?=$categoria['nome']?></br>
+                <option value="<?=$categoria['id']?>"> <?=$categoria['nome']? ></br>
+                $ehCategoria= $produto['categoria_id'] == $categoria_id;
+                $ehCategoria ? "selected = 'selected'" : "";
               <?php endforeach; ?>
             </select>
 
